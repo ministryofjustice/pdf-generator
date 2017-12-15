9c26113dbc2341b46202563f843ace9587195694
Nick Redshaw <nickredshaw@gmail.com>
2017-12-15 16:50:07 +0000

    [AM] 137 General report improvements

--- src/main/java/uk/gov/justice/digital/pdf/service/TemplateEngine.java
+++ src/main/java/uk/gov/justice/digital/pdf/service/TemplateEngine.java
@@ -5,19 +5,52 @@ import org.apache.commons.lang3.text.StrBuilder;
 import uk.gov.justice.digital.pdf.data.PdfRequest;
 import uk.gov.justice.digital.pdf.interfaces.TemplateRepository;
 
-import static java.util.Comparator.comparingInt;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static com.google.common.collect.Maps.immutableEntry;
 import static org.apache.commons.lang3.StringEscapeUtils.escapeXml10;
 
 public class TemplateEngine {
+
+    private static final Comparator<Map.Entry<String, String>> byEntryKeySize =
+            Comparator.<Map.Entry<String, String>> comparingInt(entry -> entry.getKey().length()).reversed();
 
     public static String populate(PdfRequest pdfRequest, TemplateRepository templates) {
         val document = new StrBuilder(templates.get(pdfRequest.getTemplateName()));
 
         pdfRequest.getValues()
-                .keySet()
+                .entrySet()
                 .stream()
-                .sorted(comparingInt(String::length).reversed())
-                .forEach((key) -> document.replaceAll(key, escapeXml10(pdfRequest.getValues().get(key))));
-        return document.toString();
+                .flatMap(TemplateEngine::flattenListsToArrayNotation)
+                .sorted(byEntryKeySize)
+                .forEach(entry -> document.replaceAll(entry.getKey(), escapeXml10(entry.getValue())));
+
+        return removeExcessArrayElements(pdfRequest, document.toString());
+    }
+
+    private static String removeExcessArrayElements(PdfRequest pdfRequest, String populatedTemplate) {
+        return pdfRequest.getValues()
+                .entrySet()
+                .stream()
+                .filter(entry -> entry.getValue() instanceof List)
+                .map(entry -> String.format("%s\\[%s\\]", entry.getKey(), ".*"))
+                .reduce(populatedTemplate, (template, expression) -> template.replaceAll(expression, ""));
+    }
+
+    private static Stream<? extends Map.Entry<String, String>> flattenListsToArrayNotation(Map.Entry<String, Object> entry) {
+        if (entry.getValue() instanceof List) {
+            @SuppressWarnings("unchecked")
+            List<String> listValues = (List<String>) entry.getValue();
+            return IntStream.range(0, listValues.size())
+                    .mapToObj(index -> immutableEntry(String.format("%s[%d]", entry.getKey(), index), listValues.get(index)));
+        }
+        if (entry.getValue() instanceof String) {
+            return Stream.of(immutableEntry(entry.getKey(), (String)entry.getValue()));
+        }
+        return Stream.of(immutableEntry(entry.getKey(), ""));
     }
 }
